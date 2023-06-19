1dad411924dd87b4d5e384d9e6f98e5291698a89
onurceymen <onurceymen@yahoo.com>
2023-06-19 20:08:55 +0300

    Gene Yaptık Birşeyler.

---
+++ TraversalCoreProje/ViewComponents/Default/_PopulerDestinations.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _PopulerDestinations : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
